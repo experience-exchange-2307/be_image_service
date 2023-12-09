e59f3ebdacffd28e0f5fe97af4233a6284aa3c28
Antoine Aube <aaube3@gmail.com>
2023-12-08 17:26:03 -0700

    refactor: image controller to utilize facade/service/poro

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,17 +1,10 @@
 class Api::V1::ImagesController < ApplicationController
   def index
-    conn = Faraday.new(
-      url: "https://api.unsplash.com",
-      params: { client_id: Rails.application.credentials.unsplash[:client_id] }
-    )
-
-    response = conn.get("/photos/random?query=face")
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    if parsed[:urls][:raw]
-      image = parsed[:urls][:raw]
-      render json: {image_url: "#{image}"}
+    image = ImageFacade.get_image
+    if image
+      render json: ImageSerializer.new(image), status: 200
     else
-      render json: {error: "could not retrieve a random image"}, status: 400
-    end
+      render json: {error: "image could not be found"}
+    end 
   end
 end
