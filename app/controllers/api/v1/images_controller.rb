c64424b6da9b4f3d78759d385ca154ef7c9c4440
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-12-10 16:10:01 -0700

    add error pathing

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ImagesController < ApplicationController
   def index
     image = ImageFacade.get_image
-    if image
-      render json: ImageSerializer.new(image), status: 200
+    if image[:status] == 200
+      render json: ImageSerializer.new(image[:response]), status: 200
     else
       render json: {error: "image could not be found"}, status: 404
     end 
